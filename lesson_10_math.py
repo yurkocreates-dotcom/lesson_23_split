6eeb571d3d58588c264a1280025a702bab260cb3
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 22:45:47 +0200

    lesson_11_boolean

--- lesson_10_math.py
+++
@@ -1,9 +0,0 @@
-age=int(input("Enter your age :"))
-print("Next year you will be ",age+1)
-
-current_income=int(input("Enter your current incomming :"))
-desired_income=int(input("Enter your desired incomming :"))
-
-difference=desired_income-current_income
-
-print(f'You need {difference} €  more to reach your goal')
---
+++ lesson_11_boolean.py
@@ -0,0 +1,15 @@
+is_developer= True
+
+print(is_developer)
+
+has_laptop = False
+
+print(has_laptop)
+
+
+learning_python= True
+building_projects= True
+using_github= True
+
+if learning_python and building_projects and using_github :
+    print("YES")
