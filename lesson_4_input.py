e243b76b350e0e466c6de3be7d5a79659df46782
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 14:07:08 +0200

    lesson_4_input

---
+++ lesson_4_input.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+country = input("What is your  country? ")
+
+print("Hello", name)
+print("from", country)
+
+name = input("What is your name? ")
+goal = input("What is your  goal? ")
+dream_income = input("What is your  dream_income? ")
+print("Hello", name)
+print("My goal is :", goal)
+print("My dream_income is :", dream_income)
+print("I will work every day")
