1c9a9cafde2ade46008f3f6e69f5009f3b94460c
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 22:58:39 +0200

    lesson_13_not

---
+++ lesson_13_not.py
@@ -0,0 +1,10 @@
+has_internet = False
+if not has_internet:
+     print("No connection")
+
+is_sleeping = True
+is_lazy = True
+if not is_sleeping and not is_lazy:
+     print("Time to build AI projects")
+else:
+     print("Rest and come back stronger")
