064434b7e83f5d1004d921f305c0f98451a279ca
admin <admin@iMac-von-admin.fritz.box>
2026-05-13 13:58:48 +0200

    lesson_3_functions

---
+++ ai_motivation_app.py
@@ -0,0 +1,12 @@
+def motivation():
+     print("You will be great,keep moving")
+
+
+motivation()
+def goal():
+    print("1. Push-ups")
+    print("2. Learn Python")
+    print("3. Build AI projects")
+
+
+goal()
