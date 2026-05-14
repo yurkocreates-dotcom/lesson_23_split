b044f03117584a75b45751e2c78fafbe02a55a6d
admin <admin@iMac-von-admin.fritz.box>
2026-05-14 14:11:41 +0200

    lesson_19_nested_loops

---
+++ lesson_19_nested_loops.py
@@ -0,0 +1,33 @@
+colors = [
+    "Black",
+    "White",
+    "Blue",
+    "Red"
+]
+
+clothes = [
+    "Hoodie",
+    "T-Shirt",
+    "Jacket",
+    "Jeans"
+]
+for color in colors:
+    for item in clothes:
+        print(color,item)
+
+ai_roles = [
+    "AI Engineer",
+    "Designer",
+    "Automation Developer",
+    "Content Creator"
+]
+
+countries = [
+    "Germany",
+    "Japan",
+    "USA",
+    "Canada"
+]
+for role in ai_roles:
+    for country in countries:
+        print(role,'-',country)
