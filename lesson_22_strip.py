4efdc32406a632f4233c5351a646fe6f9335f792
admin <admin@iMac-von-admin.fritz.box>
2026-05-14 15:07:48 +0200

    lesson_23_split

--- lesson_22_strip.py
+++
@@ -1,9 +0,0 @@
-city = " Mainz "
-print(city.strip())
-
-
-
-user=input('Enter your name?')
-name=user.strip()
-print(f'Clean username: {name}')
-
---
+++ lesson_23_split.py
@@ -0,0 +1,8 @@
+text = "Germany Japan Ukraine"
+words=text.split()
+print(words)
+
+user=input('enter your skills')
+skills=user.split()
+for skill in skills:
+ print(skill)
